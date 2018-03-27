2f8479bf070b1c3fd11d19e0c196642b0dce13e8
DanielM <daniel.97.molin@gmail.com>
2018-03-26 21:34:38 -0600

    source package

--- MantenimientoMiddleWare-web/src/main/java/uesocc/edu/sv/web/boundary/AbstractResource.java
+++ MantenimientoMiddleWare-web/src/main/java/uesocc/edu/sv/web/boundary/AbstractResource.java
@@ -12,7 +12,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import sv.edu.uesocc.ingenieria.prn335_2016.inventario.datos.acesso.AbstractInterface;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
