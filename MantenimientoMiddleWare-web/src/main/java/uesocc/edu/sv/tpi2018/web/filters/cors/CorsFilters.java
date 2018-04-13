cb827f01d44d6542a26d9aef9171be3399f5ed3c
DanielM <daniel.97.molin@gmail.com>
2018-04-12 23:36:49 -0600

    accesSSSSSSSS

--- MantenimientoMiddleWare-web/src/main/java/uesocc/edu/sv/tpi2018/web/filters/cors/CorsFilters.java
+++ MantenimientoMiddleWare-web/src/main/java/uesocc/edu/sv/tpi2018/web/filters/cors/CorsFilters.java
@@ -19,9 +19,9 @@ import javax.ws.rs.ext.Provider;
 public class CorsFilters implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Acces-Control-Allow-Origin", "*");
-        responseContext.getHeaders().add("Acces-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, access-control-allow-origin");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, HEAD");
     }
 }
