509e32cc6280858efa4719000b488f15f3d1a95f
DanielM <daniel.97.molin@gmail.com>
2018-06-12 21:33:13 -0600

    query de estado de solicitud con pasos completados y totales

--- MantenimientoMiddleWare-web/src/main/java/uesocc/edu/sv/tpi2018/web/boundary/SolicitudResource.java
+++ MantenimientoMiddleWare-web/src/main/java/uesocc/edu/sv/tpi2018/web/boundary/SolicitudResource.java
@@ -5,8 +5,11 @@
  */
 package uesocc.edu.sv.tpi2018.web.boundary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.json.JsonArray;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -14,7 +17,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import uesocc.edu.sv.tpi2018.ejb.controller.AbstractInterface;
 import uesocc.edu.sv.tpi2018.ejb.controller.SolicitudFacadeLocal;
+import uesocc.edu.sv.tpi2018.ejb.entities.Equipo;
 import uesocc.edu.sv.tpi2018.ejb.entities.Solicitud;
+import uesocc.edu.sv.tpi2018.web.entities.EquipoEstado;
 
 /**
  *
@@ -39,8 +44,21 @@ public class SolicitudResource extends AbstractResource<Solicitud>{
     @GET
     @Path("estado/{id}")
     @Produces(MediaType.APPLICATION_JSON + "; charset=utf-8")
-    public List<Solicitud> obtenerEstado(@PathParam("id") int id){
-        return sfl.obtenerEstado(id);
+    public List<EquipoEstado> obtenerEstado(@PathParam("id") int id){
+        List<Object[]> lista = sfl.obtenerEstado(id);
+        List<EquipoEstado> listaEquipos = new ArrayList<>();
+        System.out.println("lista de object");
+        lista.forEach( (Object[] o) -> {
+            Equipo e = (Equipo) o[0];
+            EquipoEstado es = new EquipoEstado(e, Integer.parseInt(o[1].toString()),Integer.parseInt(o[2].toString()));
+            es.setIdModelo(e.getIdModelo());
+            es.setIdUnidad(e.getIdUnidad());
+            listaEquipos.add(es);
+        });
+
+        return listaEquipos;
     }
+    
+    
     
 }
