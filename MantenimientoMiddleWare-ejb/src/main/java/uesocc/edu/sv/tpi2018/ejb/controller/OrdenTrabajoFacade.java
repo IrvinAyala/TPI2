bd79514944fcfc80b47c664d31814f3f68c0a993
Luis Doratt <luisdoratt@hotmail.com>
2018-06-14 11:41:17 -0600

    Probando hook

--- MantenimientoMiddleWare-ejb/src/main/java/uesocc/edu/sv/tpi2018/ejb/controller/OrdenTrabajoFacade.java
+++ MantenimientoMiddleWare-ejb/src/main/java/uesocc/edu/sv/tpi2018/ejb/controller/OrdenTrabajoFacade.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+s */
 package uesocc.edu.sv.tpi2018.ejb.controller;
 
 import java.util.logging.Level;
